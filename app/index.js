bd9b9d9a1f1811889aff559b53419180f60a5cf6
Jack Linton <jacklinton@mac.com>
2017-03-26 19:35:18 -0400

    This is the intial commit for the first boilerplate test of building a react native app sitting on top of a Meteor backend. The application as it stands right now is just the basic react init that has been configured to connect to the Meteor server and it has been tested to function correctly with no errors.

---
+++ app/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Meteor from 'react-native-meteor';
+
+const SERVER_URL = 'ws://localhost:3000/websocket';
+
+class App extends Component {
+  componentWillMount() {
+    Meteor.connect(SERVER_URL);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native + Meteor!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default App;
